f9fb592fdca0357dadc3ba8447d7c8c46ff85c1b
Fisselo <77895882+Fiss53@users.noreply.github.com>
2025-04-28 20:29:15 +0200

    upgrade lama sprite + ca marche pas

--- scripts/lama.gd
+++ scripts/lama.gd
@@ -11,6 +11,19 @@ extends StaticBody2D
 @export var slow_duration: int = 2 
 @export var max_health: int = 20
 
+@onready var upgrade_attack_speed_button: Button = $UpgradeMenuPanel/MarginContainer/GridContainer/AttackSpeedButton
+@onready var upgrade_slow_duration_button: Button =$UpgradeMenuPanel/MarginContainer/GridContainer/LongerSlowButton
+@onready var upgrade_slow_strength_button: Button = $UpgradeMenuPanel/MarginContainer/GridContainer/StrongerSlowButton
+@onready var placed: bool = false
+
+
+
+var current_slow_strength_upgrade = 1
+var current_attack_speed_upgrade = 1
+var current_slow_duration_upgrade = 1
+var current_lama_level = 1
+
+
 var health: int
 var enemies_in_range: Array[Node2D] = []
 
@@ -24,6 +37,15 @@ func _ready():
 	fire_timer.start()
 	fire_timer.timeout.connect(_on_fire_timer_timeout)
 	health = max_health
+
+	if (placed):
+		$Button.show()
+
+	upgrade_attack_speed_button.pressed.connect(upgrade_attack_speed)
+	upgrade_slow_strength_button.pressed.connect(upgrade_slow_duration)
+	upgrade_slow_duration_button.pressed.connect(upgrade_slow_strength)
+
+	Gamestate.money_changed.connect(check_money)
 
 func get_size() -> Vector2:
 	return Vector2($Sprite2D.texture.get_width(), $Sprite2D.texture.get_height())
@@ -95,3 +117,86 @@ func shake(duration = 0.2, strength = 5.0):
 	# Final tween to return to original position
 	tween.chain().tween_property(self, "position", original_position, 0.1)
 	tween.play()
+
+
+func upgrade_slow_strength():
+	if current_slow_strength_upgrade >= 5:
+		upgrade_slow_duration_button.disabled = true
+		print("current_hp_upgrade maxed")
+		return
+	
+	if not (Gamestate.spend_money(20)):
+		return
+	
+	slow_strength += 10
+	
+	current_slow_strength_upgrade += 1
+	check_lama_level()
+
+func upgrade_slow_duration():
+	if current_slow_duration_upgrade>= 5:
+		upgrade_slow_strength_button.disabled = true
+		print("upgrade_range maxed")
+		return
+	
+	if not (Gamestate.spend_money(20)):
+		return
+	
+	slow_duration += 1
+	
+	current_slow_duration_upgrade += 1
+	check_lama_level()
+
+func upgrade_attack_speed():
+	if current_attack_speed_upgrade >= 5:
+		upgrade_attack_speed_button.disabled = true
+		print("current_attack_speed_upgrade maxed")
+		return
+	
+	if not (Gamestate.spend_money(20)):
+		return
+		
+	fire_rate -= 0.03
+	
+	current_attack_speed_upgrade += 1
+	check_lama_level()
+
+func check_lama_level():
+	if current_lama_level >= 4:
+		return
+		
+	var total_level = current_attack_speed_upgrade + current_slow_duration_upgrade + current_slow_strength_upgrade
+	if total_level < current_lama_level*5:
+		return
+		
+	current_lama_level += 1
+	if current_lama_level == 1:
+		$Sprite2D.texture = load("res://assets/sprites/structure/herisson.png")
+	elif current_lama_level == 2:
+		$Sprite2D.texture = load("res://assets/sprites/structure/lamaUpgrade1.png")
+	elif current_lama_level == 3:
+		$Sprite2D.texture = load("res://assets/sprites/structure/lamaUpgrade2.png")
+	elif current_lama_level == 4:
+		$Sprite2D.texture = load("res://assets/sprites/structure/lamaUpgrade3.png")
+
+
+func check_money(current_money: int):
+	if current_money < 20:
+		upgrade_attack_speed_button.disabled = true
+		upgrade_slow_duration_button.disabled = true
+		upgrade_slow_strength_button.disabled = true
+	else:
+		if current_attack_speed_upgrade < 5:
+			upgrade_attack_speed_button.disabled = false
+		if current_slow_duration_upgrade < 5:
+			upgrade_slow_duration_button.disabled = false
+		if current_slow_strength_upgrade < 5:
+			upgrade_slow_strength_button.disabled = false
+
+
+func _on_button_pressed() -> void:
+	$UpgradeMenuPanel.show()
+
+
+func _on_construction_restriction_mouse_exited() -> void:
+	$UpgradeMenuPanel.hide()
